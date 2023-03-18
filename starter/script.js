a861e26910aebf2c41569cd32db5fff78d8c13af
ObianuoObi <117560960+ObianuoObi@users.noreply.github.com>
2023-03-18 19:35:16 +0000

    function attachment of onClick event thats inputs the value of the button to the search input and trigger on the search button

--- starter/script.js
+++ starter/script.js
@@ -12,3 +12,13 @@ function createButtons (array) {
             $(`<button type="button" class="cities btn btn-secondary btn-lg btn-block">${element}</button>`));
     });
 
+    // function attachment of onClick event thats inputs the value of the button to the search input and trigger on the search button
+    $("button.cities").on("click", function(event) {
+        event.preventDefault();
+        let click = $(this)[0].innerText;
+        $('#search-input').val(click);
+        $('#search-button').trigger("click");
+        $('#search-input').val('');
+    });
+}
+
