58c32ecf724763348592168aaa93f13c407e6b48
ObianuoObi <117560960+ObianuoObi@users.noreply.github.com>
2023-03-18 19:54:36 +0000

    function that change the indexes of the array returned from openweather

--- starter/script.js
+++ starter/script.js
@@ -53,6 +53,29 @@ function forecastDivData (day, image, temp, wind, humidity) {
     $('#forecast').append(weather);
 }
 
+// function that change the indexes of the array returned from openweather
+// according to the current time of the day, They are updated every 3 hours
+
+function getOpenWeatherIndexDay1 (currentHour) {
+    let index = 0
+    if (currentHour >= 0 && currentHour <= 2) {
+        index = 0;   
+    } else if (currentHour >= 3 && currentHour <= 5) {
+        index = 1;        
+    } else if (currentHour >= 6 && currentHour <= 8) {
+        index = 2;
+    } else if (currentHour >= 9 && currentHour <= 11) {
+        index = 3;
+    } else if (currentHour >= 12 && currentHour <= 14) {
+        index = 4;
+    } else if (currentHour >= 15 && currentHour <= 17) {
+        index = 5;
+    } else if (currentHour >= 18 && currentHour <= 20) {
+        index = 6;
+    } else if (currentHour >= 21 && currentHour <= 23) {
+        index = 7;
+    } return index;
+}
 
 
 
