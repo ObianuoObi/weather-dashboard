0acd9b77794ba5730a5c891ead90aa2b3407db9e
ObianuoObi <117560960+ObianuoObi@users.noreply.github.com>
2023-03-18 19:32:06 +0000

    function that creates buttons using names of cities stored in an array

--- starter/script.js
+++ starter/script.js
@@ -1,3 +1,14 @@
 // variable that stores city input
 
 let buttonName = [];
+
+// function that creates buttons using names of cities stored in an array
+
+function createButtons (array) {
+    $('#history').empty();
+
+    array.forEach(element => {
+        $('#history').append(
+            $(`<button type="button" class="cities btn btn-secondary btn-lg btn-block">${element}</button>`));
+    });
+
